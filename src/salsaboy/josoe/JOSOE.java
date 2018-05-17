3b03e9f8dfb1f8ee21b5f98155b56e09118bae8d
lpasfiel <lpasfiel@as.edu.au>
2018-05-17 18:40:20 +1000

    Added Window Options.

--- src/salsaboy/josoe/JOSOE.java
+++ src/salsaboy/josoe/JOSOE.java
@@ -1,6 +1,21 @@
 package salsaboy.josoe;
 
+import salsaboy.josoe.ui.OEFrame;
+import javax.swing.*;
+import java.awt.*;
+
 public class JOSOE {
     public static void main(String[] args) {
+        JFrame test = new JFrame();
+        test.setLayout(null);
+        OEFrame frame = new OEFrame(100, 100, "") {
+            @Override
+            public void draw(Graphics g) {
+            }
+        };
+        frame.paint(test.getGraphics());
+        
+        test.setSize(200, 200);
+        test.setVisible(true);
     }
 }
