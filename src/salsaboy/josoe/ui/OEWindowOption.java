48f22e51567bacd671b43a1dcbc0e3401a74ee92
lpasfiel <lpasfiel@as.edu.au>
2018-05-17 18:24:50 +1000

    Added Window Options.

---
+++ src/salsaboy/josoe/ui/OEWindowOption.java
@@ -0,0 +1,10 @@
+package salsaboy.josoe.ui;
+
+public class OEWindowOption {
+    public enum WindowOptions {
+        CLOSE, MINIMIZE, MAXIMIZE
+    }
+    public OEWindowOption(WindowOptions type) {
+    
+    }
+}
