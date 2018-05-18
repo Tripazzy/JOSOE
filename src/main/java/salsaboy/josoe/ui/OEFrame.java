ba5af7651b89c97249bf9f8aa493d648f3813980
lpasfiel <lpasfiel@as.edu.au>
2018-05-18 22:15:56 +1000

    Fixed the issue with not displaying.

--- src/main/java/salsaboy/josoe/ui/OEFrame.java
+++ src/main/java/salsaboy/josoe/ui/OEFrame.java
@@ -5,27 +5,31 @@ import main.java.salsaboy.josoe.Variables;
 import javax.swing.*;
 import java.awt.*;
 
-public abstract class OEFrame extends Component {
+public abstract class OEFrame extends JPanel {
     public JPanel topBar = new JPanel();
-    public boolean isMinimizable = Minimizable.class.isAssignableFrom(this.getClass());
+    public boolean isMinimizable = Minimizable.class.isAssignableFrom(this.getClass()); //This should still work on sub-classes
     
     public abstract void draw(Graphics g);
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
+        
         System.out.println("Drawing");
-        g.fillRect(getX(), getY(), 30, 30); //FIXME Why won't this run?
+        g.fillRect(getX(), getY(), getWidth(), 30);
         
         draw(g);
     }
     public OEFrame(int width, int height, @Nullable String title) {
         try {
             int inward = Variables.oeFrames.size() * 30;    //For that cascading look, similar to the one on Windows
-            setBounds(width, height, inward, inward);
+            setBounds(inward, inward, width, height);
+            
+            Variables.oeFrames.add(this);
         } catch (NullPointerException e) {
             if (!title.equals(null)) {
                 e.printStackTrace();
             } else {
-                System.out.println("STRING DON'T EXIST");
+                System.out.println("The window has no name.\nIgnoring.");
             }
         }
     }
