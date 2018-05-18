2004ea5791769b47189a94deeb9938a3b69bff9d
lpasfiel <lpasfiel@as.edu.au>
2018-05-18 22:01:30 +1000

    Fixed the issue with not displaying.

--- src/main/java/salsaboy/josoe/ui/OEFrame.java
+++ src/main/java/salsaboy/josoe/ui/OEFrame.java
@@ -15,14 +15,14 @@ public abstract class OEFrame extends JPanel {
         super.paint(g);
         
         System.out.println("Drawing");
-        g.fillRect(getX(), getY(), 30, 30); //FIXME Why won't this run?
+        g.fillRect(getX(), getY(), getWidth(), 30); //FIXME Why won't this run?
         
         draw(g);
     }
     public OEFrame(int width, int height, @Nullable String title) {
         try {
             int inward = Variables.oeFrames.size() * 30;    //For that cascading look, similar to the one on Windows
-            setBounds(width, height, inward, inward);
+            setBounds(inward, inward, width, height);
         } catch (NullPointerException e) {
             if (!title.equals(null)) {
                 e.printStackTrace();
