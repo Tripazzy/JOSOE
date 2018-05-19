f7465578aa6a3b4cdb3a0d3af11d7d502aca98e1
lpasfiel <lpasfiel@as.edu.au>
2018-05-19 10:58:57 +1000

    Removed unnecessary painting.

--- src/main/java/salsaboy/josoe/JOSOE.java
+++ src/main/java/salsaboy/josoe/JOSOE.java
@@ -12,14 +12,18 @@ public class JOSOE {
             @Override
             public void draw(Graphics g) {}
         };
+        OEFrame frame1 = new OEFrame(100, 100, "") {
+            @Override
+            public void draw(Graphics g) {
+        
+            }
+        };
         
         test.add(frame);
+        test.add(frame1);
         
         test.setSize(200, 200);
         test.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         test.setVisible(true);
-        
-        frame.paint(test.getGraphics());
-        test.repaint();
     }
 }
