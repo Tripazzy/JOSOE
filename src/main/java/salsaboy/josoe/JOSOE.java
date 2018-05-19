9ad3d9d303335a2787af9b874d48f14fe0977ca6
lpasfiel <lpasfiel@as.edu.au>
2018-05-19 13:42:56 +1000

    Added debug features

--- src/main/java/salsaboy/josoe/JOSOE.java
+++ src/main/java/salsaboy/josoe/JOSOE.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JOSOE {
-    public static void main(String[] args) {
+    private static void test() {
         JFrame test = new JFrame();
         test.setLayout(null);
         OEFrame frame = new OEFrame(100, 100, "") {
@@ -15,15 +15,22 @@ public class JOSOE {
         OEFrame frame1 = new OEFrame(100, 100, "") {
             @Override
             public void draw(Graphics g) {
-        
+            
             }
         };
-        
+    
         test.add(frame);
         test.add(frame1);
-        
+    
         test.setSize(200, 200);
         test.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         test.setVisible(true);
+    }
+    public static void main(String[] args) {
+        try {
+            if (args[0].equals("-debug")) {
+                test();
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {} //This is so that it won't cause an issue.
     }
 }
